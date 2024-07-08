7cac907b3c7c43d0b6a2ffcd09b0c1f9bf4d91c6
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-07 22:03:51 -0400

    getStartedModal done

--- frontend/my-app/components/modals/GetStartedModal.tsx
+++ frontend/my-app/components/modals/GetStartedModal.tsx
@@ -1,41 +1,52 @@
-import { Modal, Pressable, View, TextInput, BackHandler } from "react-native";
-import { router } from "expo-router";
+import {
+  Modal,
+  Pressable,
+  View,
+  TextInput,
+  useWindowDimensions,
+  useColorScheme,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { ThemedText } from "@/components/ThemedText";
-
-import { useSession } from "@/providers/session-provider";
-
 import PrimaryButton from "@/components/buttons/PrimaryButton";
-import { MaterialIcons } from "@expo/vector-icons";
-import { useEffect } from "react";
+import { ThemedView } from "@/components/ThemedView";
+
+const CloseModalX = ({
+  windowHeight,
+  windowWidth,
+  onClose,
+  color,
+}: {
+  windowHeight: number;
+  windowWidth: number;
+  onClose: () => void;
+  color: string;
+}) => {
+  return (
+    <View
+      className="absolute"
+      style={{ right: windowWidth * 0.1, top: windowHeight * 0.05 }}
+    >
+      <Pressable onPress={() => onClose()}>
+        <MaterialIcons name="close" size={25} color={color} />
+      </Pressable>
+    </View>
+  );
+};
 
 export default function HomeScreen({
   isVisible,
   onClose,
-}: {
+}: Readonly<{
   isVisible: boolean;
   onClose: () => void;
-}) {
-  useEffect(() => {
-    const backAction = () => {
-      console.log("asdsad");
-
-      if (isVisible) {
-        onClose();
-        return true;
-      }
-      return false;
-    };
-
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction,
-    );
-
-    return () => backHandler.remove();
-  }, [isVisible, onClose]);
+}>) {
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
+  const theme = useColorScheme() ?? "light";
 
   return (
     <Modal
@@ -44,44 +55,47 @@ export default function HomeScreen({
       visible={isVisible}
       onRequestClose={onClose}
     >
-      <View className="flex-1 bg-background-light">
-        <StatusBar style="light" backgroundColor="black" />
-        <View className="absolute right-[10%] top-[5%]">
-          <Pressable onPress={() => onClose()}>
-            <MaterialIcons name="close" size={25} />
-          </Pressable>
-        </View>
-        <SafeAreaView className="mt-[30%] flex-1 items-center justify-start">
-          <View className="w-[80%]">
-            <ThemedText className="mb-5 text-3xl font-bold text-text-light">
-              Lets Get Started!
-            </ThemedText>
-            <ThemedText className="mb-10 text-lg text-text-light/70">
-              Enter your email to create or sign in to your account
-            </ThemedText>
-
-            <View className="relative mb-5 h-[60] w-full rounded-xl border-2 border-black">
-              <View className="ml-2 flex-1 justify-center">
-                <ThemedText className="absolute left-0 top-0 text-xs text-text-light/50">
-                  Email
-                </ThemedText>
-                <TextInput
-                  className="text-lg"
-                  autoFocus={true}
-                  keyboardType="email-address"
-                ></TextInput>
-              </View>
-            </View>
-
-            <PrimaryButton
-              onPress={() => {
-                router.push("/sign-up");
-              }}
-              title="GET STARTED"
+      <ThemedView className="flex-1">
+        <SafeAreaView className="flex-1">
+          <View className="relative flex-1 items-center justify-start">
+            <CloseModalX
+              windowHeight={windowHeight}
+              windowWidth={windowWidth}
+              onClose={onClose}
+              color={theme === "light" ? "black" : "white"}
             />
+
+            <View className="mt-[30%] w-[80%]">
+              <ThemedText className="mb-5 text-3xl font-bold">
+                Lets Get Started!
+              </ThemedText>
+              <ThemedText className="mb-10 text-lg text-text-light/70 dark:text-text-dark/70">
+                Enter your email to create or sign in to your account
+              </ThemedText>
+
+              <View className="relative mb-5 h-[60] w-full rounded-xl border-2 border-black dark:border-white">
+                <View className="mx-2 flex-1 justify-center">
+                  <ThemedText className="absolute left-0 top-0 text-xs text-text-light/50 dark:text-text-dark/70">
+                    Email
+                  </ThemedText>
+                  <TextInput
+                    className="text-lg text-text-light dark:text-text-dark"
+                    autoFocus={true}
+                    keyboardType="email-address"
+                  ></TextInput>
+                </View>
+              </View>
+
+              <PrimaryButton
+                onPress={() => {
+                  router.push("/sign-in");
+                }}
+                title="GET STARTED"
+              />
+            </View>
           </View>
         </SafeAreaView>
-      </View>
+      </ThemedView>
     </Modal>
   );
 }
