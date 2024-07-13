da2c06643e1f2d2fbbb22740f03cae3628a0d6c2
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-13 01:50:43 -0400

    creating addContact screen

--- frontend/my-app/app/(app)/(chat)/addContact.tsx
+++ frontend/my-app/app/(app)/(chat)/addContact.tsx
@@ -1,20 +1,44 @@
-import { View, Text, Pressable } from "react-native";
-import React from "react";
+import { View, Text, Pressable, Animated } from "react-native";
+import React, { useRef, useState } from "react";
 import { router } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TopNavBar from "@/components/TopNavBar";
+import SimpleFormTextField from "@/components/form/SimpleFormTextField";
 
 type Props = {};
 
 const AddContact = (props: Props) => {
   const db = useSQLiteContext();
+
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
   return (
     <ThemedView className="flex-1">
       <SafeAreaView className="flex-1">
         <TopNavBar title="New Contact" />
-        <View className=""></View>
+        <View className="flex-1 items-center">
+          <View className="flex w-full max-w-[80%] gap-y-8 pt-4">
+            <SimpleFormTextField
+              className=""
+              title="Name"
+              value={name}
+              handleTextChange={(text) => {
+                setName(text);
+              }}
+            />
+            <SimpleFormTextField
+              className=""
+              title="Phone Number"
+              value={phoneNumber}
+              handleTextChange={(text) => {
+                setPhoneNumber(text);
+              }}
+            />
+          </View>
+        </View>
       </SafeAreaView>
     </ThemedView>
   );
