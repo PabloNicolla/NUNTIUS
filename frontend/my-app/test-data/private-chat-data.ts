c65852b22901718d873d825f1a59f5d80becce46
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-14 22:27:44 -0400

    new test data

---
+++ frontend/my-app/test-data/private-chat-data.ts
@@ -0,0 +1,19 @@
+import { PrivateChat } from "@/db/schemaTypes";
+
+export const privateChats: PrivateChat[] = [
+  { id: 1, contactId: 1, lastMessageId: undefined },
+  { id: 2, contactId: 2, lastMessageId: undefined },
+  { id: 3, contactId: 3, lastMessageId: undefined },
+  { id: 4, contactId: 4, lastMessageId: undefined },
+  { id: 5, contactId: 5, lastMessageId: undefined },
+  { id: 6, contactId: 6, lastMessageId: undefined },
+  { id: 7, contactId: 7, lastMessageId: undefined },
+  { id: 8, contactId: 8, lastMessageId: undefined },
+  { id: 9, contactId: 9, lastMessageId: undefined },
+  { id: 10, contactId: 10, lastMessageId: undefined },
+  { id: 11, contactId: 11, lastMessageId: undefined },
+  { id: 12, contactId: 12, lastMessageId: undefined },
+  { id: 13, contactId: 13, lastMessageId: undefined },
+  { id: 14, contactId: 14, lastMessageId: undefined },
+  { id: 15, contactId: 15, lastMessageId: undefined },
+];
