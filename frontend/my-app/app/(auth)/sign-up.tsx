7da6c2f2b9d233554aab56ecd6c0bc1c05de62d2
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-16 01:05:56 -0400

    sign up page validation

--- frontend/my-app/app/(auth)/sign-up.tsx
+++ frontend/my-app/app/(auth)/sign-up.tsx
@@ -4,12 +4,19 @@ import {
   TextInput,
   Image,
   useColorScheme,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Controller, useForm } from "react-hook-form";
 import { router, useLocalSearchParams } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useSession } from "@/providers/session-provider";
 
@@ -18,15 +25,39 @@ import { ThemedView } from "@/components/themed-view";
 import PrimaryButton from "@/components/buttons/primary-button";
 import FormTextField from "@/components/form/form-text-field";
 
+const formSchema = z.object({
+  email: z.string().email("Invalid Email format").min(1, "Email is required"),
+  password: z.string().min(8, "Password too short"),
+});
+
 export default function SignUpScreen() {
-  const { email } = useLocalSearchParams<{ email: string }>();
-
-  const { login, isLoggedIn } = useSession();
-
   const theme = useColorScheme() ?? "light";
 
-  const [emailValue, setEmailValue] = useState(email);
-  const [passwordValue, setPasswordValue] = useState("");
+  const { email } = useLocalSearchParams<{ email: string }>();
+
+  const { login } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: email ?? "",
+      password: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: any) => {
+    try {
+      console.log("SUBMITTING SIGN UP FORM", values);
+      form.reset();
+      login();
+      router.dismissAll();
+      router.replace("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const PasswordInputRef = useRef<TextInput | null>(null);
   const EmailInputRef = useRef<TextInput | null>(null);
@@ -49,81 +80,112 @@ export default function SignUpScreen() {
     <ThemedView className="flex-1">
       <StatusBar style="auto" />
 
-      <SafeAreaView className="flex-1">
-        <View className="flex-1 items-center justify-start">
-          <View className="w-full flex-row items-center justify-between px-2">
-            <View className="flex-row items-center">
-              <Pressable
-                onPress={() => {
-                  router.back();
-                }}
-              >
-                <MaterialIcons
-                  name="arrow-back"
-                  size={30}
-                  color={theme === "light" ? "black" : "white"}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+        className=""
+      >
+        <SafeAreaView className="flex-1">
+          <View className="flex-1">
+            <View className="w-full flex-row items-center justify-between px-2">
+              <View className="flex-row items-center">
+                <Pressable
+                  onPress={() => {
+                    router.back();
+                  }}
+                >
+                  <MaterialIcons
+                    name="arrow-back"
+                    size={30}
+                    color={theme === "light" ? "black" : "white"}
+                  />
+                </Pressable>
+                <Image
+                  source={require("@/assets/images/brand/Logo.png")}
+                  className="ml-4 h-[60] w-[60]"
                 />
-              </Pressable>
-              <Image
-                source={require("@/assets/images/brand/Logo.png")}
-                className="ml-4 h-[60] w-[60]"
-              />
+              </View>
+              <View className="items-center">
+                <Pressable
+                  onPress={() => {
+                    router.replace("/sign-in");
+                  }}
+                >
+                  <ThemedText className="text-lg font-bold">Log in</ThemedText>
+                </Pressable>
+              </View>
             </View>
-            <View className="items-center">
-              <Pressable
-                onPress={() => {
-                  router.replace("/sign-in");
-                }}
-              >
-                <ThemedText className="text-lg font-bold">Log in</ThemedText>
-              </Pressable>
-            </View>
+
+            <ScrollView
+              contentContainerStyle={{
+                alignItems: "center",
+              }}
+              className=""
+            >
+              <View className="w-[80%] pt-[10%]">
+                <ThemedText className="mb-5 text-3xl font-bold">
+                  Lets Get Started!
+                </ThemedText>
+                <ThemedText className="mb-10 text-lg text-text-light/70 dark:text-text-dark/70">
+                  Create your account
+                </ThemedText>
+
+                <Controller
+                  control={form.control}
+                  name="email"
+                  disabled={isLoading}
+                  render={({
+                    field: { value, onChange, onBlur },
+                    fieldState: { error },
+                  }) => (
+                    <FormTextField
+                      ref={EmailInputRef}
+                      className="mb-5"
+                      handleTextChange={onChange}
+                      title="Email"
+                      value={value}
+                      error={error}
+                      isLoading={isLoading}
+                      keyboardType="email-address"
+                    />
+                  )}
+                />
+
+                <Controller
+                  control={form.control}
+                  name="password"
+                  disabled={isLoading}
+                  render={({
+                    field: { value, onChange, onBlur },
+                    fieldState: { error },
+                  }) => (
+                    <FormTextField
+                      ref={PasswordInputRef}
+                      className="mb-5"
+                      handleTextChange={onChange}
+                      title="Password"
+                      value={value}
+                      isSecureText={true}
+                      titleTransformX={16}
+                      onBlur={onBlur}
+                      error={error}
+                      isLoading={isLoading}
+                      keyboardType="default"
+                    />
+                  )}
+                />
+
+                <PrimaryButton
+                  className="mb-20"
+                  handlePress={form.handleSubmit((data: any) => onSubmit(data))}
+                  title="CREATE ACCOUNT"
+                  isLoading={isLoading}
+                />
+              </View>
+            </ScrollView>
           </View>
-
-          <View className="mt-[10%] w-[80%]">
-            <ThemedText className="mb-5 text-3xl font-bold">
-              Lets Get Started!
-            </ThemedText>
-            <ThemedText className="mb-10 text-lg text-text-light/70 dark:text-text-dark/70">
-              Create your account
-            </ThemedText>
-
-            <FormTextField
-              ref={EmailInputRef}
-              className="mb-5"
-              title="Email"
-              value={emailValue}
-              handleTextChange={(text) => {
-                setEmailValue(text);
-              }}
-            />
-
-            <FormTextField
-              ref={PasswordInputRef}
-              className="mb-5"
-              title="Password"
-              value={passwordValue}
-              handleTextChange={(text) => {
-                setPasswordValue(text);
-              }}
-              isSecureText={true}
-              titleTransformX={16}
-            />
-
-            <PrimaryButton
-              handlePress={() => {
-                console.log(isLoggedIn, "before");
-                login();
-                console.log(isLoggedIn, "after");
-
-                router.dismissAll();
-                router.replace("/");
-              }}
-              title="CREATE ACCOUNT"
-            />
-          </View>
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
     </ThemedView>
   );
 }
