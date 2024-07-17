2a3ad79148d02f595eddc79ee5347223953804e0
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-16 23:26:08 -0400

    chat screen

--- frontend/my-app/app/(app)/(chat)/chat/[id].tsx
+++ frontend/my-app/app/(app)/(chat)/chat/[id].tsx
@@ -1,24 +1,102 @@
+import TopNavBarChat from "@/components/custom-nav-bar/top-nav-bar-chat";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { Message, PrivateChat } from "@/db/schemaTypes";
+import { getFirstPrivateChat } from "@/db/statements";
+import { messages } from "@/test-data/message-data";
+import { Ionicons } from "@expo/vector-icons";
 import { useLocalSearchParams } from "expo-router";
-import { StyleSheet } from "react-native";
+import { useSQLiteContext } from "expo-sqlite";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Pressable,
+  StyleSheet,
+  TextInput,
+  useColorScheme,
+  View,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function ChatScreen() {
   const { id } = useLocalSearchParams();
+  const [chat, setChat] = useState<PrivateChat | null>(null);
+  const theme = useColorScheme();
 
-  console.log(id);
+  const db = useSQLiteContext();
+
+  useEffect(() => {
+    console.log("ChatScreen", Number(id));
+    async function getChatAndContact() {
+      const chat = await getFirstPrivateChat(db, Number(id));
+      if (!chat) {
+        console.log("TopNavBarChat ERROR invalid chatId");
+      }
+      setChat(chat);
+    }
+    getChatAndContact();
+  }, []);
+
+  const renderItem = ({ item, index }: { item: Message; index: number }) => {
+    return (
+      <View className={`w-full ${item.id % 2 === 0 ? "items-end" : ""}`}>
+        <ThemedText>{item.value}</ThemedText>
+      </View>
+    );
+  };
 
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText>Details of user {id} </ThemedText>
+    <ThemedView className="flex-1">
+      <SafeAreaView className="flex-1">
+        <TopNavBarChat contactId={chat?.contactId} />
+        <View className="relative flex-1">
+          <FlatList
+            data={messages}
+            renderItem={renderItem}
+            // ListFooterComponent={ListFooterComponent}
+            ListHeaderComponent={
+              <HeaderComponent handleSendMessage={() => {}} />
+            }
+            indicatorStyle={theme === "dark" ? "white" : "black"}
+            showsHorizontalScrollIndicator={true}
+            inverted={true}
+          />
+        </View>
+      </SafeAreaView>
     </ThemedView>
   );
 }
+const HeaderComponent = ({
+  handleSendMessage,
+}: {
+  handleSendMessage: (query: string) => void;
+}) => {
+  const theme = useColorScheme();
+  const [messageValue, setMessageValue] = useState("");
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+  return (
+    <View className="w-full items-center justify-center">
+      <View className="my-2 h-12 w-[95%] rounded-3xl bg-black/5 px-4 dark:bg-white/10">
+        <Pressable className="flex-1 flex-row items-center" onPress={() => {}}>
+          <Ionicons
+            size={20}
+            name="send"
+            color={theme === "dark" ? "white" : "black"}
+          />
+          <TextInput
+            className="ml-2 flex-1 text-text-light dark:text-text-dark"
+            placeholder="Message..."
+            placeholderTextColor={
+              theme === "dark" ? "rgba(225,232,249,0.7)" : "rgba(6,13,30,0.7)"
+            }
+            numberOfLines={1}
+            value={messageValue}
+            onChangeText={(text) => {
+              setMessageValue(text);
+            }}
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
---
+++ frontend/my-app/components/custom-nav-bar/top-nav-bar-chat.tsx
@@ -0,0 +1,99 @@
+import { Image, Pressable, useColorScheme, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ThemedView } from "../themed-view";
+import { router } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
+import { ThemedText } from "../themed-text";
+import { Avatar } from "react-native-paper";
+import { useAvatarModal } from "@/providers/avatarModal-provider";
+import { useSQLiteContext } from "expo-sqlite";
+import { Contact, PrivateChat } from "@/db/schemaTypes";
+import { getFirstContact, getFirstPrivateChat } from "@/db/statements";
+
+type Props = {
+  contactId?: number;
+};
+
+const TopNavBarChat = ({ contactId }: Props) => {
+  const theme = useColorScheme() ?? "light";
+  const [contact, setContact] = useState<Contact | null>(null);
+  const db = useSQLiteContext();
+
+  useEffect(() => {
+    console.log("TopNavBarChat", contactId);
+    async function getChatAndContact() {
+      if (contactId) {
+        const contact = await getFirstContact(db, contactId);
+        if (!contact) {
+          console.log("TopNavBarChat ERROR invalid contactId");
+        }
+        setContact(contact);
+      }
+    }
+    getChatAndContact();
+  }, [contactId, db]);
+
+  return (
+    <ThemedView className="h-14 w-full flex-row items-center border-b-[1px] border-primary-light/50 px-2">
+      <Pressable
+        className="mr-4"
+        onPress={() => {
+          router.back();
+        }}
+      >
+        <MaterialIcons
+          name="arrow-back"
+          size={30}
+          color={theme === "light" ? "black" : "white"}
+        />
+      </Pressable>
+      <CustomAvatar
+        username={contact?.name ?? ""}
+        imageURl={contact?.imageURL}
+      />
+      <ThemedText className="pl-4">{contact?.name}</ThemedText>
+    </ThemedView>
+  );
+};
+
+const CustomAvatar = ({
+  username,
+  imageURl,
+}: {
+  username: string;
+  imageURl?: string;
+}) => {
+  const [imageError, setImageError] = useState(false);
+  const { showModal } = useAvatarModal();
+
+  const getInitials = (name: string) => {
+    const initials = name
+      .split(" ")
+      .map((word) => word.charAt(0))
+      .join("");
+    return initials.slice(0, 2).toUpperCase();
+  };
+
+  return (
+    <View>
+      <Pressable
+        onPress={() => {
+          if (imageURl) showModal(imageURl ?? "");
+        }}
+      >
+        {imageError || !imageURl ? (
+          <Avatar.Text label={getInitials(username)} size={40} />
+        ) : (
+          <Image
+            source={{ uri: imageURl }}
+            style={{ width: 40, height: 40, borderRadius: 25 }}
+            onError={() => setImageError(true)}
+            resizeMode="cover"
+          />
+        )}
+      </Pressable>
+    </View>
+  );
+};
+
+export default TopNavBarChat;
