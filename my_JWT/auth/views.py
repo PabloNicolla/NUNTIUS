a38103829b5d5370c3b6d9bfbd6177508847a880
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-28 04:37:39 -0400

    new settings for JWT backend (test)

--- my_JWT/auth/views.py
+++ my_JWT/auth/views.py
@@ -8,39 +8,17 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = UserSerializer(data=request.data)
+        print(request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class CustomAuthToken(ObtainAuthToken):
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.serializer_class(data=request.data,
-#                                            context={'request': request})
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data['user']
-#         token, created = Token.objects.get_or_create(user=user)
-#         return Response({
-#             'token': token.key,
-#             'user_id': user.pk,
-#             'email': user.email
-#         })
-
-
-# class LogoutView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request):
-#         request.user.auth_token.delete()
-#         return Response(status=status.HTTP_200_OK)
-
-# views.py
 
 
 class LogoutView(APIView):
@@ -59,6 +37,7 @@ class LogoutView(APIView):
 class PublicView(APIView):
     permission_classes = (AllowAny,)
 
+    @csrf_exempt
     def get(self, request):
         return Response({"message": "This is a public endpoint"})
 
--- my_JWT/my_JWT/views.py
+++ my_JWT/my_JWT/views.py
@@ -1,21 +1,57 @@
-from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from dj_rest_auth.registration.views import RegisterView, SocialLoginView
+from django.conf import settings
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework import status
 
 
-# if you want to use Authorization Code Grant, use this
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://127.0.0.1:8000/accounts/google/login/callback/"
-    client_class = OAuth2Client
+
+    def get_response(self):
+        serializer_class = self.get_response_serializer()
+        serializer = serializer_class(instance=self.token)
+        data = serializer.data
+
+        refresh = RefreshToken.for_user(self.user)
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        response = Response(data, status=status.HTTP_200_OK)
+        return response
 
 
-class GoogleLogin(SocialLoginView):  # if you want to use Implicit Grant, use this
-    adapter_class = GoogleOAuth2Adapter
+class CustomRegisterView(RegisterView):
+    # def get_response_data(self, user):
+    #     data = super().get_response_data(user)
+    #     refresh = RefreshToken.for_user(user)
+    #     access_token = str(refresh.access_token)
+    #     # Prepare response data
+    #     response_data = {
+    #         'user': data,
+    #         'access_token': access_token,
+    #         'refresh_token': str(refresh)
+    #     }
+    #     return response_data
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        if response.status_code == 201:
+            # Extract response data
+            response_data = response.data
 
-
-class GitHubLogin(SocialLoginView):
-    adapter_class = GitHubOAuth2Adapter
-    callback_url = CALLBACK_URL_YOU_SET_ON_GITHUB
-    client_class = OAuth2Client
+            # Set cookies
+            response.set_cookie(
+                settings.REST_AUTH['JWT_AUTH_COOKIE'],
+                response_data['access'],
+                httponly=settings.REST_AUTH['JWT_AUTH_HTTPONLY'],
+                samesite=settings.REST_AUTH['JWT_AUTH_SAMESITE']
+            )
+            response.set_cookie(
+                settings.REST_AUTH['JWT_AUTH_REFRESH_COOKIE'],
+                response_data['refresh'],
+                httponly=settings.REST_AUTH['JWT_AUTH_HTTPONLY'],
+                samesite=settings.REST_AUTH['JWT_AUTH_SAMESITE']
+            )
+        return response
