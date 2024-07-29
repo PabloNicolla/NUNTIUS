d2ab9a1419209a0c0ff88b5473dc603da70a143b
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-07-29 16:59:15 -0400

    deleting test backends

--- backend2/testingRest/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import views
-
-urlpatterns = [
-    re_path("", views.test_hello)
-]
--- my_JWT/user/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import CheckEmailView
-
-urlpatterns = [
-    path('check-email/', CheckEmailView.as_view(), name='check-email'),
-]
