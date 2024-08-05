d1a7c13ea60fb4c4918ef264eb9a8856e5f5b76e
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-04 22:38:51 -0400

    adding `/` route to django app so it is easier to create healthy checks

--- backend/user_app/urls.py
+++ backend/user_app/urls.py
@@ -3,6 +3,7 @@ from .views import CheckEmailView, ContactView, ProfileImageView, health_check
 
 urlpatterns = [
     path('health/', health_check),
+    path('', health_check),
     path('api/v1/user/check-email/', CheckEmailView.as_view(), name='check-email'),
     path('api/v1/user/contact/', ContactView.as_view(), name='contact'),
     path('api/v1/user/profile/imageURL/',
